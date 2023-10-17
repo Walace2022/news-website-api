b0affeaef011c527430e34a2f04ee48c1ed86c80
Henrique Walace <Henriquewalace@hotmail.com>
2023-10-17 01:21:58 -0300

    Added enviroment variables

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import express from "express";
-const app = express();
-
 //Banco de dados
 import connectDatabase from "./src/database/db.js";
-
 //Routes
 import userRoute from "./src/routes/user.route.js";
+
+import dotenv from "dotenv";
+const app = express();
+const port = process.env.PORT || 3000;
+dotenv.config();
 
 connectDatabase();
 app.use(express.json());
 app.use("/user", userRoute);
 
-app.listen(3000);
+app.listen(port);
--- src/database/db.js
+++ src/database/db.js
@@ -4,9 +4,7 @@ const connectDatabase = () => {
   console.log("Esperando conectar com o Banco de dados.");
 
   mongoose
-    .connect(
-      "mongodb+srv://BrazLibrary:KmqbfCN0OBSm5M50@cluster0.50wburc.mongodb.net/?retryWrites=true&w=majority"
-    )
+    .connect(process.env.MONGODB_URI)
     .then(console.log("Conectado ao Banco de dados com Sucesso."))
     .catch((error) => {
       console.log(error);
