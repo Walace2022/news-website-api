42e7926bc4e8d89566181a4849c29dbb0931ecef
Henrique Walace <Henriquewalace@hotmail.com>
2023-10-25 22:04:10 -0300

    Added pagination to get news

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,9 @@
-import { createService, findAllService } from "../services/news.service.js";
+import { text } from "express";
+import {
+  createService,
+  findAllService,
+  countNews,
+} from "../services/news.service.js";
 
 const create = async (req, res) => {
   try {
@@ -23,13 +28,56 @@ const create = async (req, res) => {
 
 const findAll = async (req, res) => {
   try {
-    const news = await findAllService();
+    let { limit, offset } = req.query;
+
+    limit = Number(limit);
+    offset = Number(offset);
+
+    if (!limit) {
+      limit = 5;
+    }
+
+    if (!offset) {
+      offset = 0;
+    }
+
+    const news = await findAllService(offset, limit);
+    const total = await countNews();
+    const currentUrl = req.baseUrl;
+
+    const next = offset + limit;
+    const nextUrl =
+      next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+    const previous = offset - limit < 0 ? null : offset - limit;
+    const previousUrl =
+      previous != null
+        ? `${currentUrl}?limit=${limit}&offset=${previous}`
+        : null;
+
     if (news.length === 0) {
       return res
         .status(404)
         .send({ message: "Nenhuma noticia publicada no momento." });
     }
-    res.send(news);
+    res.send({
+      nextUrl,
+      previousUrl,
+      limit,
+      offset,
+      total,
+      results: news.map((item) => ({
+        id: item._id,
+        title: item.title,
+        text: item.text,
+        banner: item.banner,
+        likes: item.likes,
+        comments: item.comments,
+        name: item.user.name,
+        username: item.user.username,
+        userAvatar: item.user.avatar,
+      })),
+    });
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
